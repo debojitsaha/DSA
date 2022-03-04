78e0fa5232c5309f0bd813d745157bd409e122f9
debojit <saha05debojit@gmail.com>
2022-03-04 21:25:16 +0530

    Right Left Top & Bottom Views

--- BinaryTree_Views.cpp
+++ BinaryTree_Views.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<queue>
+#include<map>
 using namespace std;
 
 struct Node
@@ -17,6 +18,9 @@ struct Node
 
 void rightView(Node* root)
 {
+    if(root==NULL)
+        return;
+
     queue<Node*> q;
     q.push(root);
     while(!q.empty())
@@ -38,6 +42,9 @@ void rightView(Node* root)
 
 void leftView(Node* root)
 {
+    if(root==NULL)
+        return;
+
     queue<Node*> q;
     q.push(root);
     while(!q.empty())
@@ -57,6 +64,70 @@ void leftView(Node* root)
     }
 }
 
+class Pair                  // used in Top & Bottom View Level Queues
+{
+    public:
+    int hd;
+    Node* node;    
+    Pair(int h, Node* nod)
+    {
+        hd=h;
+        node= nod;
+    }
+};
+
+void topView(Node* root)
+{
+    if(root==NULL)
+        return;
+
+    queue<Pair> q;
+    map<int, int> M;
+    
+    q.push(Pair(0, root));
+    while(!q.empty())
+    {        
+        Pair curr= q.front();
+        q.pop();
+        
+        if(M.find(curr.hd)==M.end())        
+            M[curr.hd]= curr.node->data;                    
+        
+        if(curr.node->left!=NULL)    
+            q.push(Pair(curr.hd-1, curr.node->left));
+        if(curr.node->right!=NULL)
+            q.push(Pair(curr.hd+1, curr.node->right));        
+    }
+    
+    for(auto& i:M)
+        cout<<i.second<<" ";
+}
+
+void bottomView(Node* root)
+{
+    if(root==NULL)
+        return;
+
+    queue<Pair> q;
+    map<int,int> M;
+    q.push(Pair(0,root));
+    while(!q.empty())
+    {
+        Pair curr= q.front();
+        q.pop();
+
+        M[curr.hd]= curr.node->data;
+
+        if(curr.node->left!=NULL)
+            q.push(Pair(curr.hd-1,curr.node->left));
+        if(curr.node->right!=NULL)
+            q.push(Pair(curr.hd+1,curr.node->right));        
+    }
+
+    for(auto& i:M)
+        cout<<i.second<<" ";
+}
+
 int main()
 {
     Node* root= new Node(1);
@@ -68,6 +139,16 @@ int main()
     root->right->right= new Node(7);
     root->left->left->left= new Node(8);
 
+    /*  The above Binary Tree is as follows:           
+                     1
+                   /   \
+                  2     3
+                 / \   / \
+                4   5  6  7
+               /
+              8
+    */
+
     cout<<"Right View of Binary Tree: ";
     rightView(root);
     cout<<endl;
@@ -76,6 +157,13 @@ int main()
     leftView(root);
     cout<<endl;
 
+    cout<<"Top View of Binary Tree: ";
+    topView(root);
+    cout<<endl;
 
+    cout<<"Bottom View of Binary Tree: ";
+    bottomView(root);
+    cout<<endl;       
+    
     return 0;
 }
